9fa896a7710073d6d86cd25ca812c92524bdf1c1
Yves Legris <ylegris94@gmail.com>
2018-02-21 22:46:07 -0500

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# NodeJS with Polymer
+
+## Features
+
+* Routing with Polymer and secured by NodeJS
+* Login with `JWT` (JSON Web Token)
+
+## Setup
+Command to setup the project
+
+Install `bower` package for Polymer components
+```
+bower install
+```
+
+Install `npm` package for NodeJS module
+```
+npm install
+```
+
+Launch app
+```
+node server.js
+```
+
+By default app is lauched on `port 3000`
--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 // Node.js notation for importing packages
 var express = require('express');
+var config = require('./config');
 var jwt = require('jsonwebtoken');
 
 // Spin up a server
 var app = express();
 
 // Serve static files from the main build directory
-app.use(express.static(__dirname + '/build/default'));
+app.use(express.static('public'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 // Check specific page
 app.get('/question', isLogin, function (request, response) {
     console.log('Question.');
-    response.sendFile('index.html', { root: '.', headers: { page: request.originalUrl } });
+    response.sendFile(config.appPath, { root: '.', headers: { page: request.originalUrl } });
 });
 
 app.post('/login', function(request, response) {
-    var token = jwt.sign({'sub': 'login', 'iss': 'Yves Legris', 'aud': 'admin'}, 'secret');
+    var token = jwt.sign({'sub': 'login', 'iss': 'John Doe', 'aud': 'admin'}, config.secret);
 
     response.json({
         token: token
@@ -24,12 +26,13 @@ app.post('/login', function(request, response) {
 
 // Render index.html on the main page, specify the root
 app.get('*', function (request, response) {
-    response.sendFile('index.html', { root: '.', headers: { page: request.originalUrl } });
+    console.log(request.originalUrl);
+    response.sendFile(config.appPath, { root: '.', headers: { page: request.originalUrl } });
 });
 
 // Tell the app to listen for requests on port 3000
-app.listen(3000, function () {
-    console.log('Listening on port 3000');
+app.listen(config.port, function () {
+    console.log('Listening on port ' + config.port);
 });
 
 // Middleware to check if user is connected
@@ -37,10 +40,9 @@ function isLogin(request, response, next) {
     console.log('Check Token.');
 
     try {
-        var decoded = jwt.verify(request.header('authorization'), 'secret');
-        console.log(decoded);
+        jwt.verify(request.header('authorization'), config.secret);
         next();
     } catch(error) {
-        response.sendFile('index.html', { root: '.', headers: { page: '404' } });
+        response.sendFile(config.appPath, { root: '.', headers: { page: '404' } });
     }
 }
